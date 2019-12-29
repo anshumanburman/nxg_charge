5308ede13e1e92b5b785c0b8f088108a9853accc
unknown <anshumank13@gmail.com>
2019-12-29 23:00:58 +0530

    jyotish rai changeg

--- src/navigations/drawerNavigator.js
+++ src/navigations/drawerNavigator.js
@@ -3,6 +3,7 @@ import * as Style from '../styles';
 import Home from '../components/screens/Home';
 import CustomDrawer from '../components/screens/CustomDrawer';
 import ChargingStation from '../components/screens/ChargingStation';
+import Offers from '../components/screens/Offers';
 
 
 
@@ -12,6 +13,7 @@ import ChargingStation from '../components/screens/ChargingStation';
 const DrawerNavigator = createDrawerNavigator({
     [Style.Constants.KEY_HOME]: Home,
     [Style.Constants.KEY_CHARGING_STATIONS]: ChargingStation,
+    [Style.Constants.KEY_OFFERS] : Offers,
 }, {
     initialRouteName: Style.Constants.KEY_HOME,
     contentComponent: CustomDrawer
